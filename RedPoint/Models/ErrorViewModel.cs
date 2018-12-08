e9c40f7002381162837eda93102eb527860fe858
Arkadiusz Kołodziejek <arkko002@gmail.com>
2018-12-08 01:08:17 +0100

    Add project files.

---
+++ RedPoint/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RedPoint.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
