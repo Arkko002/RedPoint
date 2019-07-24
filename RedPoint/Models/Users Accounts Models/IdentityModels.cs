2b9746e46e2bab5b0bf9ba5b4490532b5696fe72
Arkadiusz Kołodziejek <arkko002@gmail.com>
2019-07-25 00:44:44 +0200

    Change channel id to string

--- RedPoint/Models/Users Accounts Models/IdentityModels.cs
+++ RedPoint/Models/Users Accounts Models/IdentityModels.cs
@@ -3,18 +3,35 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using RedPoint.Models.Chat_Models;
 using Microsoft.EntityFrameworkCore;
+using RedPoint.Models.Chat_Models.User_Settings;
 
 namespace RedPoint.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string CurrentChannelId { get; set; }
+        public int CurrentServerId { get; set; }
+
         public List<Message> Messages { get; set; }
         public List<Group> Groups { get; set; }
         public List<Server> Servers { get; set; }
-        public int CurrentChannelId { get; set; }
-        public int CurrentServerIId { get; set; }
         public UserStub UserStub { get; set; }
+        public UserSettings UserSettings { get; set; }
+
+        //public ApplicationUser()
+        //{
+        //    CreateUserStub();
+        //}
+
+        //private void CreateUserStub()
+        //{
+        //    UserStub = new UserStub()
+        //    {
+        //        AppUserId = Id,
+        //        AppUserName = UserName
+        //    };
+        //}
     }
 
 
