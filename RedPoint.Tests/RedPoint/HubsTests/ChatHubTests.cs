c654ef0a0001314a8995a4cc7da5e6dd0a7e9154
Arkadiusz Kołodziejek <arkko002@gmail.com>
2019-07-25 01:18:25 +0200

    Add missing test files

--- RedPoint.Tests/RedPoint/HubsTests/ChatHubTests.cs
+++ RedPoint.Tests/RedPoint/HubsTests/ChatHubTests.cs
@@ -1,82 +1,101 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using NUnit.Framework;
 using RedPoint.Data;
 using RedPoint.Hubs;
+using RedPoint.Infrastructure;
 using RedPoint.Models;
-using Moq;
-using Microsoft.AspNetCore.SignalR;
-using System.Dynamic;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using RedPoint.Models.Chat_Models;
+using RedPoint.Tests.RedPoint.Identity;
 
-namespace RedPoint.Tests.RedPoint.HubTests
+namespace RedPoint.Tests.RedPoint.HubsTests
 {
     [TestFixture]
     public class ChatHubTests
     {
         private ApplicationDbContext _db;
-        private UserManager<ApplicationUser> _userManager;
+        private Mock<FakeUserManager> _userManager;
         private ChatHub _chatHub;
+        private Mock<HubUserInputValidator> _inputValidator;
 
-        [SetUp]
+        [OneTimeSetUp]
         protected void SetUp()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: "ChatHubTestDb")
-                .Options;
-                
+                .UseInMemoryDatabase("ChatHubTestDb")
+                .Options;            
             _db = new ApplicationDbContext(options);
 
-            var userStore = new Mock<IUserStore<ApplicationUser>>();
-            _userManager = new UserManager<ApplicationUser>(userStore.Object, null, null, null,
-                                                            null, null, null, null, null);
 
+            var userId = Guid.NewGuid().ToString();
+            var users = new List<ApplicationUser>
+            {
+                new ApplicationUser()
+                {
+                    UserName = "Test",
+                    Id = userId,
+                    Email = "test@test.com",
+                    
+                    UserStub = new UserStub()
+                    {
+                        AppUserId = userId,
+                        AppUserName = "Test"
+                    }
+                    
+                }
+            }.AsQueryable();
 
+            _userManager = new Mock<FakeUserManager>();
+            _userManager.Setup(u => u.Users).Returns(users);
 
-            _chatHub = new ChatHub(_db, _userManager);
+            _inputValidator = new Mock<HubUserInputValidator>();
 
+            _chatHub = new ChatHub(_db, _userManager.Object, _inputValidator.Object);
 
             Server server = new Server()
             {
                 Name = "TestServer",
                 Channels = new List<Channel>()
             };
-
+            
             server.Channels.Add(new Channel()
             {
                 Name = "Channel_1",
-                Description = String.Empty,
+                Description = string.Empty,
                 ChannelStub = new ChannelStub()
                 {
                     Name = "Channel_1",
-                    Description = String.Empty,
+                    Description = string.Empty,
                 }
             });
+
+            _db.Servers.Add(server);
+            _db.SaveChanges();
         }
 
         [Test]
         public async Task Send()
         {
             //arrange
-            bool sendCalled = false;
-            dynamic all = new ExpandoObject();
-            all.AddNewMessage = new Action<Models.Chat_Models.Message>((message => { sendCalled = true; }));
-
             Mock<IHubCallerClients> mockClients = new Mock<IHubCallerClients>();
             Mock<IClientProxy> mockClientProxy = new Mock<IClientProxy>();
             mockClients.Setup(c => c.All).Returns(mockClientProxy.Object);
 
-            _chatHub.Clients = (IHubCallerClients<IChatHub>) mockClients.Object;
+            _chatHub.Clients = (IHubCallerClients<IChatHub>)mockClients.Object;
+
+            //TODO Mock ChatHub context and connection ID (Identity)
 
             //act
             await _chatHub.Send("TestMessage", "Channel_1");
 
             //assert
-            Assert.IsTrue(sendCalled);
+
         }
 
         [Test]
@@ -92,9 +111,34 @@ namespace RedPoint.Tests.RedPoint.HubTests
         }
 
         [Test]
-        public void ServerChanged()
+        public async Task ServerChanged()
         {
+            //TODO Make testable hub without strong types
 
+
+            //arrange
+            Mock<IHubCallerClients<IChatHub>> mockClients = new Mock<IHubCallerClients<IChatHub>>();                     
+            Mock<HubCallerContext> mockContext = new Mock<HubCallerContext>();
+            Mock<IClientProxy> mockProxy = new Mock<IClientProxy>();
+
+            //Mock<IHubContext<ChatHub>> mockCon = new Mock<IHubContext<ChatHub>>();
+
+            bool serverChanged = false;
+            dynamic caller = new ExpandoObject();
+            caller.getChannelList = new Action<List<Channel>>((list) => { serverChanged = true; });
+
+            mockContext.Setup(c => c.UserIdentifier).Returns("1");
+            //mockClients.Setup(c => c.All).Returns(mockProxy.Object);
+
+            _chatHub.Clients = mockClients.Object;
+            _chatHub.Context = mockContext.Object;
+
+            //act
+            await _chatHub.ServerChanged(1);
+
+            //assert
+            mockClients.Verify(c => c.Caller, Times.Once);
+            Assert.True(serverChanged);
         }
     }
 }
