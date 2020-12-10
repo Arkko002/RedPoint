9605d8385be08ccd17abb588f62fcffa99b44a13
Arkadiusz Kołodziejek <arkko002@gmail.com>
2020-12-10 22:27:51 +0100

    Docs update

--- RedPoint.Account/Services/IAccountService.cs
+++ RedPoint.Account/Services/IAccountService.cs
@@ -8,7 +8,18 @@ namespace RedPoint.Account.Services
     /// </summary>
     public interface IAccountService
     {
+        /// <summary>
+        /// Attempts to log in user with the provided data.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>JWT token on successful login.</returns>
         Task<string> Login(UserLoginDto model);
+        
+        /// <summary>
+        /// Attempts to register user with the provided data.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>JWT token on successful register.</returns>
         Task<string> Register(UserRegisterDto model);
         Task<bool> Delete(UserLoginDto model);
     }
