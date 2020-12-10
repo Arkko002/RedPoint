9fe4ee53f50f3e4842b83cc04fcae43f88854f97
Arkadiusz Kołodziejek <arkko002@gmail.com>
2020-12-10 22:26:36 +0100

    Use MariaDB as a database provider. Create initial migrations.

--- RedPoint.Account/Startup.cs
+++ RedPoint.Account/Startup.cs
@@ -2,7 +2,6 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using RedPoint.Account.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,6 +13,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 //TODO NLog config file
 [assembly: ApiController]
@@ -32,8 +33,14 @@ namespace RedPoint.Account
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AccountDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("AccountDb")));
+                options.UseMySql(
+                        Configuration.GetConnectionString("AccountDb"), 
+                        new MariaDbServerVersion(new Version(10, 5, 8)),
+                        mySqlOptions => mySqlOptions
+                            .CharSetBehavior(CharSetBehavior.NeverAppend))
+                    .EnableDetailedErrors()
+                    .EnableSensitiveDataLogging()
+            );
 
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<AccountDbContext>()
@@ -44,6 +51,7 @@ namespace RedPoint.Account
                 options.Password.RequireDigit = true;
                 options.Password.RequireLowercase = true;
                 options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
                 options.Password.RequiredLength = 6;
                 options.Password.RequiredUniqueChars = 1;
 
@@ -54,7 +62,7 @@ namespace RedPoint.Account
 
                 options.User.AllowedUserNameCharacters =
                     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-                options.User.RequireUniqueEmail = true;
+                options.User.RequireUniqueEmail = false;
             });
 
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
@@ -69,18 +77,25 @@ namespace RedPoint.Account
                 options.SaveToken = true;
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    ValidIssuer = Configuration["JwtIssuer"],
-                    ValidAudience = Configuration["JwtIssuer"],
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
                     IssuerSigningKey =
                         new SymmetricSecurityKey(
-                            Encoding.UTF8.GetBytes(Configuration["JwtKey"])), // TODO!!! set proper key
+                            Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])), // TODO!!! set proper key
                     ClockSkew = TimeSpan.Zero
                 };
             });
 
             services.AddControllers();
-            services.AddScoped(typeof(IAccountService), typeof(AccountService));
-            services.AddScoped(typeof(IAccountRequestValidator), typeof(AccountRequestValidator));
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IAccountRequestValidator, AccountRequestValidator>();
+            services.AddScoped<ITokenGenerator, JwtTokenGenerator>();
+            services.AddSingleton<IAccountErrorHandler, AccountErrorHandler>();
+            services.AddSingleton<IAccountSecurityConfigurationProvider, AccountSecurityConfigurationProvider>();
 
         }
 
@@ -99,8 +114,6 @@ namespace RedPoint.Account
             }
             
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
             app.UseRouting();
 
             app.UseAuthentication();
@@ -110,7 +123,7 @@ namespace RedPoint.Account
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller}/{action}");
                 endpoints.MapRazorPages();
             });
         }
